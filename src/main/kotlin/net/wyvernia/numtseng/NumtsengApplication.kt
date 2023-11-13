81af0b90a3512b61fc3ec29c8c74d99fbfb76ffc
João Braz <joao.braz@merkle.com>
2023-11-13 10:30:17 +0000

    Initial generation

---
+++ src/main/kotlin/net/wyvernia/numtseng/NumtsengApplication.kt
@@ -0,0 +1,11 @@
+package net.wyvernia.numtseng
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NumtsengApplication
+
+fun main(args: Array<String>) {
+    runApplication<NumtsengApplication>(*args)
+}
---
+++ src/test/kotlin/net/wyvernia/numtseng/NumtsengApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.wyvernia.numtseng
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NumtsengApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
